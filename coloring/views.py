5dd94927036a00208442b41d06b8c8806aa2164d
Kathy Hoang <khoang21@berkeley.edu>
2021-07-17 10:25:17 -0700

    finished project 3 by adding all linked templates and drawing screen

--- coloring/views.py
+++ coloring/views.py
@@ -6,9 +6,55 @@ def homepage(request):
     return render(request, 'coloring/homepage.html')
 def search(request):
     return render(request, 'coloring/search.html')
-def coloringpage(request):
-    return render(request, 'coloring/coloringpage.html')
+def coloringhome(request):
+    return render(request, 'coloring/coloringhome.html')
 def new(request):
     return render(request, 'coloring/new.html')
+def popular(request):
+    return render(request, 'coloring/popular.html')
+def destress(request):
+    return render(request, 'coloring/destress.html')
 def animals(request):
     return render(request, 'coloring/animals.html')
+def autumn(request):
+    return render(request, 'coloring/autumn.html')
+def cute(request):
+    return render(request, 'coloring/cute.html')
+def disney(request):
+    return render(request, 'coloring/disney.html')
+def fashion(request):
+    return render(request, 'coloring/fashion.html')
+def fruit(request):
+    return render(request, 'coloring/fruit.html')
+def floral(request):
+    return render(request, 'coloring/floral.html')
+def hobby(request):
+    return render(request, 'coloring/hobby.html')
+def love(request):
+    return render(request, 'coloring/love.html')
+def mandala(request):
+    return render(request, 'coloring/mandala.html')
+def music(request):
+    return render(request, 'coloring/music.html')
+def nature(request):
+    return render(request, 'coloring/nature.html')
+def people(request):
+    return render(request, 'coloring/people.html')
+def places(request):
+    return render(request, 'coloring/places.html')
+def pixar(request):
+    return render(request, 'coloring/pixar.html')
+def random(request):
+    return render(request, 'coloring/random.html')
+def sports(request):
+    return render(request, 'coloring/sports.html')
+def spring(request):
+    return render(request, 'coloring/spring.html')
+def summer(request):
+    return render(request, 'coloring/summer.html')
+def thanksgiving(request):
+    return render(request, 'coloring/thanksgiving.html')
+def winter(request):
+    return render(request, 'coloring/winter.html')
+def zendoodle(request):
+    return render(request, 'coloring/zendoodle.html')
